2a3438ddb127c6d4d61783305485732a13920e82
chang_su <chang_su@hunliji.com>
2016-09-22 16:43:16 +0800

    react helloworld test

--- app/index.js
+++ app/index.js
@@ -1,7 +1,4 @@
 import 'whatwg-fetch'
-import 'antd/dist/antd.min.css'
-import './style/antd/index.css'
-import './style/index.css'
 import React from 'react'
 import {render} from 'react-dom'
 import App from './container/App'
