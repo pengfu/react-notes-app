16a7cdbe414f839c1ae5ba12735c42fe5dc3e24a
chang_su <chang_su@hunliji.com>
2016-09-22 20:02:51 +0800

    nodelist

--- app/components/NotesList/index.js
+++ app/components/NotesList/index.js
@@ -7,7 +7,37 @@ export default class extends Component{
 
     render(){
         return(
-            <div>srst</div>
+            <div id="notes-list">
+
+                <div id="list-header">
+                    <h2>Notes | pengfoo</h2>
+                    <div className="btn-group btn-group-justified" role="group">
+
+                        <div className="btn-group" role="group">
+                            <button type="button" className="btn btn-default">
+                                All Notes
+                            </button>
+                        </div>
+
+                        <div className="btn-group" role="group">
+                            <button type="button" className="btn btn-default">
+                                Favorites
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="list-group">
+                        <a  className="list-group-item" href="#">
+                            <h4 className="list-group-item-heading">
+                                tesrtertetetet
+                            </h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
